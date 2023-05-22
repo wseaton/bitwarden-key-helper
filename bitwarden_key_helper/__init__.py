01651d1891a23e7f42fb831cb0be944259433f80
Will Eaton <me@wseaton.com>
2023-05-22 13:32:07 -0400

    first commit

---
+++ bitwarden_key_helper/__init__.py
@@ -0,0 +1,67 @@
+import subprocess
+from getpass import getpass
+from typing import List
+
+from serde.json import from_json
+
+from bw_types import PasswordEntry
+
+
+def remove_duplicate_sequences(byte_string, separator=b"\x1b[37D\x1b[37C\x1b[2K\x1b[G"):
+    """
+    Seperator here is a magic sequence of escape characters that the `bw` cli
+    uses to redraw the password prompt after every character is typed.
+    """
+    sequences = byte_string.split(separator)
+
+    seen = set()
+    deduplicated_sequences = []
+
+    for seq in sequences:
+        if seq not in seen:
+            seen.add(seq)
+            deduplicated_sequences.append(seq)
+
+    # we drop the first because the escape sequence in our case is slightly
+    # different the first go round which produces two final values
+    return separator.join(deduplicated_sequences[1:])
+
+
+def search_bw_one(search: str) -> str:
+    """
+    Wrap the `bw` bitwarden CLI to run a search command interactively.
+
+    Returns the raw password
+    """  # noqa: E501
+    command = ["bw", "get", "password", search]
+    process = subprocess.Popen(
+        command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    input_str = getpass("bitwarden master password:")
+    output, errors = process.communicate(input_str.encode())
+    # The process' output and errors are also in bytes, so we decode them.
+    output = output.decode()
+    # TODO: maybe do something smart conditionally on errors
+    errors = remove_duplicate_sequences(errors).decode()
+    return output
+
+
+def search_bw(search: str) -> List[PasswordEntry]:
+    """
+    Wrap the `bw` bitwarden CLI to run a search command interactively.
+
+    Returns a list of PasswordEntry objects
+    """  # noqa: E501
+    command = ["bw", "list", "items", "--search", search]
+    process = subprocess.Popen(
+        command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+
+    input_str = getpass("bitwarden master password:")
+    output, errors = process.communicate(input_str.encode())
+
+    # The process' output and errors are also in bytes, so we decode them.
+    output = output.decode()
+    errors = remove_duplicate_sequences(errors).decode()
+
+    return from_json(List[PasswordEntry], output)
---
+++ bitwarden_key_helper/bw_types.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+from serde import SerdeSkip, field, serde
+
+from datetime import datetime
+
+from typing import Optional, List
+
+
+def serializer(cls, o):
+    if cls is datetime:
+        return o.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    else:
+        raise SerdeSkip()
+
+
+def deserializer(cls, o):
+    if cls is datetime:
+        return datetime.strptime(o, "%Y-%m-%dT%H:%M:%S.%fZ")
+    else:
+        raise SerdeSkip()
+
+
+@serde(serializer=serializer, deserializer=deserializer, rename_all="camelcase")
+@dataclass
+class Login:
+    username: str
+    password: str
+    totp: Optional[str]
+    password_revision_date: datetime
+
+
+@serde(serializer=serializer, deserializer=deserializer, rename_all="camelcase")
+@dataclass
+class PasswordHistory:
+    last_used_date: datetime = field(rename="lastUsedDate")
+    password: str
+
+
+@serde(serializer=serializer, deserializer=deserializer, rename_all="camelcase")
+@dataclass
+class PasswordEntry:
+    _id: str = field(rename="id")
+    organization_id: str
+    folder_id: str
+    _type: int = field(rename="type")
+    reprompt: int
+    name: str
+    notes: Optional[str]
+    favorite: bool
+    login: Login
+
+    collection_ids: List[str]
+    revision_date: datetime
+    creation_date: datetime
+    deleted_date: Optional[datetime]
+    passwordHistory: List[PasswordHistory]
